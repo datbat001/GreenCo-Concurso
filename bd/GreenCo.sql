6c04f62e1b021cd8a5f328433d94476f0453147c
datbat <davidalejandro1240@gmail.com>
2022-06-21 15:05:35 -0500

    creacion de las tablas para la base de datos

---
+++ bd/GreenCo.sql
@@ -0,0 +1,247 @@
+Create database GreenCo_gps;
+
+use GreenCo_gps;
+
+create table Categorias(
+   idCategoria int not null auto_increment,
+   nomCategoria char (64) not null unique,
+   primary key(idCategoria)
+);
+
+
+CREATE TABLE Productos (
+	idProducto INT NOT NULL AUTO_INCREMENT,
+	nomProd char(64),
+	-- precio float, ESTE VALOR TIENE QUE SER CALCULADO, POR QUE ES PROVISTO POR MULTIPLES PROVEEDORES ADEMÁS DEBERÁS DE MOSTRAR UN RANGO DE PRECIOS
+	descripcion varchar(128),	
+	existencia int comment "ES LA SUMA DE TODOS LOS PRODUCTOS",
+	PRIMARY KEY (IdProducto)
+);
+
+
+create table Tags(
+   idTag int not null auto_increment,
+   idCategoria int not null,
+   nomTag char(16) not null unique,
+   primary key (idTag),
+   foreign key (idCategoria) references Categorias (idCategoria)
+);
+
+create table TagsProds(
+   idProducto int not null ,
+   idTag int not null,
+   primary key(idProducto, idTag),
+   foreign key (idProducto) references Productos (idProducto),
+   foreign key (idTag) references Tags (idTag)
+);
+
+
+CREATE TABLE Estados (
+    idEstado INT NOT NULL AUTO_INCREMENT,
+    nomEstado CHAR(64) NOT NULL unique,
+    PRIMARY KEY (idEstado)
+);
+
+create table Municipios(
+   idMunicipio int not null auto_increment,
+   idEstado int not null,
+   nomMunicipio char(128),
+   primary key(idMunicipio, idEstado),
+   foreign key (idEstado) references Estados (idEstado)   
+);
+
+create table Asentamientos(
+   idAsentamiento int not null auto_increment,
+   idMunicipio int not null,
+   nomAsentamiento char(128),
+   primary key(idAsentamiento, idMunicipio),
+   foreign key (idMunicipio) references Municipios (idMunicipio)   
+);
+
+CREATE TABLE Colonias (
+    idColonia INT NOT NULL AUTO_INCREMENT,
+    idAsentamiento INT NOT NULL,
+    nomColonia CHAR(128) NOT NULL,
+    PRIMARY KEY (idColonia , idAsentamiento),
+    FOREIGN KEY (idAsentamiento) REFERENCES Asentamientos (idAsentamiento)
+);
+
+CREATE TABLE Calles (
+    idCalle INT NOT NULL AUTO_INCREMENT,
+    idColonia int,
+    nomCalle CHAR(128) NOT NULL,
+    PRIMARY KEY (idCalle, idColonia),
+    foreign key (idColonia) references Colonias (idColonia)
+);
+
+/*
+Ejemplo Json:
+
+{
+"edificio": "2",
+"piso": "2",
+"lote": "3",
+"Entre calle 1": "tlacoapan",
+"Entre calle 2": "temoaya",
+"tipoDeVivienda": {
+"condominio": "False",
+"vivienda": "True",
+"residencial": "False",
+"fraccionamiento": "False"
+}
+}
+
+*/
+
+CREATE TABLE Usuarios(
+    idUsuario INT NOT NULL AUTO_INCREMENT,
+    nickname char(64) not null unique comment 'ES EL NOMBRE DE USUARIO PARA INGRESAR P.EJ. GOLOSA69',
+    nomUsuario char(64) not null,
+    apellidos varchar(128),
+    fechaNacimiento DATE,
+    rutaFotoUsuario VARCHAR(2048),
+    idCalle int,
+    num varchar(16),
+    datosExtraDireccion json,	
+    PRIMARY KEY (idUsuario),
+    FOREIGN KEY (idCalle)
+        REFERENCES Calles (idCalle)
+);
+
+CREATE TABLE RolesDeUsuarios(
+	idRol int auto_increment,
+    rolUsuario char(5) comment "usuario, vendMin, tienda, admin, superadmin",    
+	PRIMARY KEY (idRol)
+);
+
+create table UsuariosRoles(
+   idRol int not null,
+   idUsuario int not null,
+   primary key (idRol, idUsuario),
+   foreign key (idRol) references RolesDeUsuarios (idRol),
+   foreign key (idUsuario) references Usuarios (idUsuario)
+);
+
+create table FormasDeContacto(
+   idContacto int, 
+   formaDeContacto char(128),
+   tipoContacto char(4) comment "Tel, Cel, Mail, Twit, Face...",
+   primary key (idContacto, formaDeContacto),
+   foreign key (idContacto) references Usuarios (idUsuario)
+);
+
+create table FotosProds(
+   idProducto int not null,
+   rutaFotoProducto varchar(2048),
+   fechaSubida datetime,
+   validada boolean,
+   publica boolean comment "SI NO ES PÚBLICA ENTONCES NECESITA UN DUEÑO",
+   idPropietario int comment "PERMITE SUBIR FOTOS PERSONALIZADA PARA UN PRODUCTO QUE ESTÁ DADO DE ALTA Y SÓLO APARECERÁ EN LA TIENDA DEL USUARIO",
+   foreign key (idProducto) references Productos(idProducto),
+   foreign key (idPropietario) references Usuarios (idUsuario)
+);
+
+create table DatosFacturacion(   
+   idDatosFacturacion int auto_increment,
+   idCliente int,
+   RFC char (13),
+   razonSocial varchar (512),
+   datosDelCliente bool comment "Utiliza los datos de direccion del cliente",
+   idCalle int,
+   num varchar(16),
+   datosExtraDireccion json,	   
+   primary key (idDatosFacturacion),
+   key(idCliente,RFC),
+   foreign key (idCliente) references Usuarios (idUsuario)
+);
+
+create table TagsUsuarios(
+   idUsuario int not null ,
+   idTag int not null,
+   primary key(idUsuario, idTag),
+   foreign key (idUsuario) references Usuarios(idUsuario),
+   foreign key (idTag) references Tags (idTag)
+);
+
+
+CREATE TABLE Carrito(
+	idCarrito INT NOT NULL AUTO_INCREMENT,
+    idCliente int,
+    idDatosFacturacion int,
+	fechaVenta DATETIME,
+	-- impuesto FLOAT, viola la quinta FN
+	-- Total FLOAT comment "Por la fluctuación de precios",
+	estadoVenta char(10) not null default "comprando" comment "retenido, en revisión, incompleta, en progreso, pagado, cancelado, envianda, denegado, entregado",
+    estadoEnvio char(10) NOT NULL DEFAULT 'enBodega' comment "Empacando, Enviando, EnCamino, Llegando, Recibido, Cancelado, Retornado, Disputa",    
+    formaDePago char(5) comment "credito, debito, trans, deposito",    
+	PRIMARY KEY (idCarrito, idCliente),    
+    foreign key (idDatosFacturacion) references DatosFacturacion (idDatosFacturacion),
+	FOREIGN KEY (idCliente) REFERENCES Usuarios(idUsuario)
+);
+
+create table ProductosEnElCarrito(
+   idProducto int,
+   idCarrito int,
+   costoVenta float,
+   cantidad float,
+   descuento float comment "porcentaje de descuento",
+   primary key(idProducto, idCarrito),
+   foreign key (idProducto) references Productos (idProducto),
+   foreign key (idCarrito) references Carrito(idCarrito)
+);
+
+
+create table TagsCarrito(
+   idCarrito int not null ,
+   idTag int not null,
+   primary key(idCarrito, idTag),
+   foreign key (idCarrito) references Carrito (idCarrito),
+   foreign key (idTag) references Tags (idTag)
+);
+
+
+create table InventarioProveedores(
+   idProdProv int not null auto_increment,
+   idProveedor int not null,
+   idProducto int not null,
+   costo float not null,
+   primary key (idProdProv),
+   key (idProveedor,idProducto),
+   foreign key (idProveedor) references Usuarios(idUsuario),
+   foreign key (idProducto) references Productos (idProducto)
+);
+
+create table PreciosProds(
+   idProdProv int not null,
+   precio float,
+   fechaActualizacion datetime,
+   validada boolean,
+   primary key(idProdProv, fechaActualizacion) comment "EL COSTO ES EL ÚLTIMO QUE SE ACTUALIZÓ", 
+   foreign key (idProdProv) references InventarioProveedores(idProdProv)
+);
+
+
+
+/*
+describe usuarios;
+/*31/marzo vistas/
+create view usuarios_registro as select idUsuario,
+NombreUser,Apellido from usuarios where Direccion is null;
+
+create view carrito_prod_guard as select cantidad 
+from ProductosEnElCarrito as ProdC inner join productos
+as P on ProdC.idProducto= P.idProducto inner join carrito 
+as C on ProdC.idCarrito= C.idCarrito where cantidad=10;
+
+create view prod_org_mariscos as select nombrePro, precio
+from productos where precio > 50; 
+*/
+
+
+-- <Inserts> 
+
+
+
+
+
+--  </Inserts>

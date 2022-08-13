c6f5368d6eface6a3697b535552a3fcb7ce4964c
datbat <davidalejandro1240@gmail.com>
2022-08-13 17:25:38 -0500

    creacion del perfil de usuario

---
+++ GreenCo/app/src/main/java/com/itz/isc/greenco/perfil.java
@@ -0,0 +1,14 @@
+package com.itz.isc.greenco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}

26e7857dda1a4b0624e5847449ad4029d2355c18
datbat001 <davidalejandro1240@gmail.com>
2022-09-02 22:31:39 -0500

    creacion de tablas en la base de datos

--- GreenCo/app/src/main/java/com/itz/isc/greenco/sqlite/greenco.java
+++ GreenCo/app/src/main/java/com/itz/isc/greenco/sqlite/greenco.java
@@ -1,5 +1,7 @@
 package com.itz.isc.greenco.sqlite;
 
+import java.sql.Date;
+
 /**
  * Clase que establece los nombres a usar en la base de datos
  * **/
@@ -23,11 +25,45 @@ public class greenco {
     }
     interface Municipios{
         String IdMunicipio = "IdMunicipio";
+        String Id_Estado = "Id_Estado";
+        String NomMunicipio = "NomMunicipio";
+
     }
-    interface ColumnasCabeceraPedido {
-        String ID = "id";
-        String FECHA = "fecha";
-        String ID_CLIENTE = "id_cliente";
-        String ID_FORMA_PAGO = "id_forma_pago";
+    interface Asentamientos{
+        String IdAsentamiento = "IdAsentamientos";
+        String Id_Municipio = "Id_Municipio";
+        String NomAsentamiento = "NomAsentamiento";
+
     }
+    interface Colonias{
+        String IdColonia = "IdColonia";
+        String Id_Asentamiento = "Id_Asentamiento";
+        String NomColonia = "NomColonia";
+    }
+    interface Calles{
+        String IdCalle = "IdCalle";
+        String Id_Colonia = "Id_Colonia";
+        String NomCalle = "NomCalle";
+    }
+   interface Usuarios{
+        String IdUsuario = "IdUsuario";
+        String Nickname = "Nickname";
+        String NomUsuario = "NomUsuario";
+        String ApPaterno = "ApPaterno";
+        String ApMaterno = "ApMaterno";
+        String FechaNac = "FechaNac";
+        String RutaFoto = "RutaFotoUsuario";
+        String Id_calle = "Id_calle";
+        String num = "num";
+        String DE = "DatosExtra";
+   }
+   interface RolesDeUsuarios{
+        String IdRol = "IdRol";
+        String RolUsuario = "RolUsuario";
+   }
+   interface FotosProds{
+        String Id_Producto = "Id_Producto";
+        String RutaFotoProd = "RutaFotoProd";
+        String FechaSubida = "FechaSubida";
+   }
 }

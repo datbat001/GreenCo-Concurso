0d5272757210ea46d531b754e69bda02453c4ed2
datbat001 <76512448+datbat001@users.noreply.github.com>
2022-09-25 16:49:03 -0500

    Revert "navbar"
    This reverts commit d2b9a300bbe8f461253d6d1d222e482ef7ff4af2, reversing
    changes made to 37fa6189f1caeca6022178636430ffb79eafdd76.

--- GreenCo/app/src/main/java/com/itz/isc/greenco/MainActivity.java
+++ GreenCo/app/src/main/java/com/itz/isc/greenco/MainActivity.java
@@ -3,61 +3,26 @@ package com.itz.isc.greenco;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-<<<<<<< HEAD
 import android.widget.TextView;
-=======
-import android.widget.Toast;
->>>>>>> ee994c9b4f89b86db3ae1f2b3dc1f928c0b79117
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.android.material.navigation.NavigationView;
 import com.itz.isc.greenco.Carrito.carrito;
-import com.itz.isc.greenco.LOGIN.login;
-import com.itz.isc.greenco.fragmentos.Beneficios;
-import com.itz.isc.greenco.fragmentos.Promociones;
-import com.itz.isc.greenco.fragmentos.Verificacion;
-import com.itz.isc.greenco.fragmentos.favoritos;
 
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    DrawerLayout drawerLayout;
-    Toolbar  toolbar;
-    NavigationView navigationView;
-    ActionBarDrawerToggle drawerToggle;
-    ImageButton btnExperiencias, btnBebidas, btnIngredientes,
-            btnPanaderia,btnProductos,btnCongelados,btnCarnes,
-            btnMariscos,btnshop,profile;
-
-
-
-<<<<<<< HEAD
     ImageButton btnExperiencias, btnBebidas, btnIngredientes,btnPanaderia,btnProductos,btnCongelados,btnCarnes,btnMariscos,btnshop;
     TextView txtXp,txtBebidas,txtIngre,txtPan,txtProd,txtCon,txtCar,txtMar;
 
-=======
->>>>>>> ee994c9b4f89b86db3ae1f2b3dc1f928c0b79117
 
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
 
-<<<<<<< HEAD
         txtXp = findViewById(R.id.txtXp);
         txtBebidas = findViewById(R.id.txtBed);
         txtIngre = findViewById(R.id.txtIng);
@@ -75,43 +40,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         btnProductos = findViewById(R.id.btnprod);
         btnMariscos = findViewById(R.id.btnmar);
         btnshop = findViewById(R.id.btnShop);
-=======
->>>>>>> ee994c9b4f89b86db3ae1f2b3dc1f928c0b79117
 
-            btnBebidas = findViewById(R.id.btnbeb);
-            btnCarnes = findViewById(R.id.btncarnes);
-            btnCongelados = findViewById(R.id.btncong);
-            btnExperiencias = findViewById(R.id.btnxp);
-            btnIngredientes = findViewById(R.id.btning);
-            btnPanaderia = findViewById(R.id.btnpan);
-            btnProductos = findViewById(R.id.btnprod);
-            btnMariscos = findViewById(R.id.btnmar);
-            btnshop = findViewById(R.id.btnShop);
-            //---------------navigation----------------------
-            profile = findViewById(R.id.imageHeader);
-
-            toolbar = findViewById(R.id.toolbar);
-
-            drawerLayout = findViewById(R.id.drawer_main);
-
-            navigationView = findViewById(R.id.nav_view);
-            navigationView.setNavigationItemSelectedListener(this);
-            navigationView.setItemIconTintList(null);
-
-            drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,
-                    R.string.Open,R.string.Close);
-
-
-            drawerLayout.addDrawerListener(drawerToggle);
-
-            drawerToggle.syncState();
-
-            if(savedInstanceState == null){
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new favoritos()).commit();
-                navigationView.setCheckedItem(R.id.Opcion_Favoritos);
+        btnshop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shop(view);
             }
-<<<<<<< HEAD
         });
         btnExperiencias.setOnClickListener(view -> {
             String str = txtXp.getText().toString();
@@ -167,115 +101,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
-=======
-
-
-            //---------------Botones del main----------------------
-            btnshop.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    shop(view);
-                }
-            });
-            btnExperiencias.setOnClickListener(view -> {
-                String str = null;
-                Intent intent = new Intent(getApplicationContext(), showProductos.class);
-                intent.putExtra("Experiencias", str);
-                startActivity(intent);
-            });
-            btnBebidas.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Bebidas(view);
-                }
-            });
-            btnIngredientes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Ingredientes(view);
-                }
-            });
-            btnPanaderia.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Panaderia(view);
-                }
-            });
-            btnProductos.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Productos(view);
-                }
-            });
-            btnCongelados.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Congelados(view);
-                }
-            });
-            btnCarnes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Carnes(view);
-                }
-            });
-            btnMariscos.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Mariscos(view);
-                }
-            });
-
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        try {
-
-        int id = item.getItemId();
-
-        if(id == R.id.Opcion_Favoritos){
-
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new favoritos()).commit();
-
-        }else if (id == R.id.Opcion_Beneficios){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new Beneficios()).commit();
-        }
-        else if (id == R.id.Opcion_Promociones){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new Promociones()).commit();
-        }
-        else if (id == R.id.Opcion_Verificaion){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new Verificacion()).commit();
-        }
-        else if (id == R.id.Opcion_Salir){
-            startActivity(new Intent(getApplicationContext(),login.class));
-            Toast.makeText(this,"Cerraste sesion",Toast.LENGTH_SHORT).show();
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return true;
-    }
-
-
-
-    /*public void experiencias(View view){
-            String str= "";
-            Intent intent = new Intent();
-
-            startActivity(new Intent(getApplicationContext(), showProductos.class));
-            intent.putExtra("Experiencias",str);
-        }*/
->>>>>>> ee994c9b4f89b86db3ae1f2b3dc1f928c0b79117
     public void Bebidas(View view){
         startActivity(new Intent(getApplicationContext(), showProductos.class));
     }public void Ingredientes(View view){
@@ -294,9 +119,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void shop(View view){
         startActivity(new Intent(getApplicationContext(), carrito.class));
     }
-
-
-
-
-
 }
